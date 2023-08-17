7d0b493c8540a866a4425c028000905fd95d6c1c
lauti3314 <lautaro3314@hotmail.com>
2023-08-17 09:16:55 -0300

    ejercicios complementarios hechos

---
+++ index.py
@@ -0,0 +1,46 @@
+#Ejercicios complementarios 
+numero1 = 2
+numero2 = 2.5
+
+suma = numero1 + numero2
+resta= numero2 - numero1
+multiplicacion= numero1 * numero2
+division= numero2 / numero1
+
+nombre = "Lautaro"
+
+precio = 10.5
+descuento = 0.5
+precio_final = precio * descuento
+
+cadena = "hola mundo"
+longitud = len(cadena)
+
+precio2 = 0.5 
+num_entero = int(precio2)
+
+nombre2 = "Roger"
+apellido = "Federer"
+nombre_completo = nombre2 + " " + apellido
+
+edad = 20 + 5 - 10
+
+altura = (1.80 * 4) / 3
+
+nombre_mayuscula = "LAUTARO"
+nom_min = nombre_mayuscula.lower()
+nom_mayus_min = nombre_mayuscula.title()
+
+#Impresiones por pantalla
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(precio_final)
+print(longitud)
+print(num_entero)
+print(nombre_completo)
+print(altura)
+print(nom_min)
+print(nom_mayus_min)
