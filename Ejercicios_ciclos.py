12ad3248e7cda29fa008a12d86a25c6687e812ea
lauti3314 <lautaro3314@hotmail.com>
2023-08-29 09:42:39 -0300

    ejercicios completos

--- Ejercicios_ciclos.py
+++ Ejercicios_ciclos.py
@@ -12,3 +12,34 @@ for i in range(6):
     print(mensaje_encriptado)
 
 #ejercicio 2
+
+numero = input("Ingrese el numero: ")
+numero_string = str(numero)
+numero = int(numero_string)
+
+total_par=0
+total_impar=0
+
+while numero > 0:
+    pares = 0
+    impares = 0
+    if len(numero_string) > 1:
+        for i in numero_string:
+            if int(i)%2 == 0:
+                pares += 1
+                total_par +=1
+            else:
+                impares +=1
+                total_impar +=1
+    else: 
+        if numero%2 == 0:
+                pares += 1
+                total_par +=1
+        else:
+                impares +=1
+                total_impar +=1
+    print(f"El numero de digitos pares para el numero {numero} es de {pares} y de digitos impares es {impares}")
+    numero = input("Ingrese el numero: ")
+    numero_string = str(numero)
+    numero = int(numero_string)
+print(f"El numero total de dígitos pares es de {total_par} y de digitos impares es {total_impar}")
