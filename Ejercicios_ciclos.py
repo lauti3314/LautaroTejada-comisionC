2bdf6cfd32156cb8a86cfb4254616c349e192058
lauti3314 <lautaro3314@hotmail.com>
2023-08-29 09:44:57 -0300

    cambios nuevos

--- Ejercicios_ciclos.py
+++ Ejercicios_ciclos.py
@@ -2,7 +2,7 @@
 letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
 cantidad = int(input("Ingrese la cantidad de lugares que recorre cada letra: "))
 
-for i in range(6):
+for i in range(5):
     frase =input(f"Ingrese la frase {i}: ").lower()
     mensaje_encriptado=""
     for letra in frase:
