d3ae8270a19b689d3364a8d98e0431ee786a59f2
lauti3314 <lautaro3314@hotmail.com>
2023-08-29 09:19:46 -0300

    ejercicio 1 hecho

--- Ejercicios_ciclos.py
+++ Ejercicios_ciclos.py
@@ -1,9 +1,14 @@
 #ejercicio 1
 letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
 cantidad = int(input("Ingrese la cantidad de lugares que recorre cada letra: "))
+
 for i in range(6):
     frase =input(f"Ingrese la frase {i}: ").lower()
+    mensaje_encriptado=""
     for letra in frase:
        posicion= letras.index(letra)
        nueva_letra= (cantidad + posicion)%27
-       
+       mensaje_encriptado = mensaje_encriptado + letras[nueva_letra] 
+    print(mensaje_encriptado)
+
+#ejercicio 2
