23b29b5fa6718f77d5e2ba8430564c0a6130de12
lauti3314 <lautaro3314@hotmail.com>
2023-09-19 09:37:39 -0300

    ejercicio 1 terminado

---
+++ funciones.py
@@ -0,0 +1,15 @@
+#Archivo de funciones
+
+def most(a,b):
+    if a>b:
+        return a
+    else : 
+        return b 
+
+def least(a,b):
+    if a < b:
+        return a
+    else:
+        return b
+    
+#Funciones del ahorcado
---
+++ programa_funciones.py
@@ -0,0 +1,12 @@
+#Programa main de ejercicio de funciones
+
+#Ejercicio funciones 1
+"""
+from funciones import most, least
+
+x = int(input('Un número: '))
+y = int(input('Otro número: '))
+
+print(most(x-3,least(x+2, y-5)))
+"""
+#Ejericio ahorcado
