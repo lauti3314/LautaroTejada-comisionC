1a82276c1c4c0d3d723dd9be2e474c893b99fc77
lauti3314 <lautaro3314@hotmail.com>
2023-10-23 21:12:37 -0300

    tp 7 terminado

--- TP 7/tp7.py
+++ TP 7/tp7.py
@@ -1,4 +1,28 @@
 import tp7_funciones
+#1
+numbers_list = []
+
+print("Ingrese 5 numeros y luego seran ordenados:")
+
+for i in range(5):
+    number = int(input(f"Ingrese numero {i+1}: "))
+    numbers_list.append(number)
+
+sort_list = tp7_funciones.numbers_bubble_sort(numbers_list)
+
+print(f"La lista ordenada es: {sort_list}")
+#2
+word_list = []
+
+print("Ingrese una lista de palabras para ordenarla alfabeticamente: ")
+
+for i in range(5):
+    word = input(f"Palabra {i+1}: ")
+    word_list.append(word)
+
+list_word_sort = tp7_funciones.caracter_selection_sort(word_list)
+
+print(list_word_sort)
 #3
 
 books = [
@@ -33,10 +57,30 @@ books_sort = tp7_funciones.sort_books_by_field(books, field_sort)
 for book in books_sort:
     print(f'Título: {book["titulo"]}, Autor: {book["autor"]}, Año de Publicación: {book["anio_publicacion"]}')
 
-
 #4
+word_list = []
 
+print("Ingrese una lista de palabras para ordenarla alfabeticamente: ")
+
+for i in range(5):
+    word = input(f"Palabra {i+1}: ")
+    word_list.append(word)
+
+list_word_sort = tp7_funciones.caracter_selection_sort(word_list)
+
+print(list_word_sort)
 #5
+word_list = []
+
+print("Ingrese una lista de palabras para ordenarla alfabeticamente: ")
+
+for i in range(5):
+    word = input(f"Palabra {i+1}: ")
+    word_list.append(word)
+
+list_word_sort = tp7_funciones.caracter_selection_sort(word_list)
+
+print(list_word_sort)
 #6
 
 numbers = [4, 2, 2, 8, 3, 3, 1]
@@ -46,6 +90,17 @@ print("Lista original:", numbers)
 print("Lista ordenada:", list_sort)
 
 #7
+print("Ingrese elementos en una lista luego sera ordenada primero por numeros y luego alfabeticamente.")
+
+items_list = []
+
+for i in range(5):
+    element = str(input(f"Ingrese elemento {i+1}: "))
+    items_list.append(element)
+
+items_list_sort = tp7_funciones.items_list_sort(items_list)
+
+print(f"La lista ordenada es: {items_list_sort}")
 #8
 
 numbers = [38, 27, 43, 3, 9, 82, 10]
--- TP 7/tp7_funciones.py
+++ TP 7/tp7_funciones.py
@@ -1,6 +1,51 @@
+#1
+def numbers_bubble_sort(list):
+
+    large_list = len(list)
+    for i in range(large_list):
+        for j in range(large_list-1):
+            if list[j] > list[j+1]:
+                var = list[j]
+                list[j] = list[j+1]
+                list[j+1] = var
+    return list
+#2
+def caracter_selection_sort(list):
+    large_list = len(list)
+    for i in range(large_list):
+        min_index = i
+        for j in range(i, large_list):
+            if (list[j][0]) < (list[min_index][0]): 
+                var = list[j]
+                list[j] = list[min_index]
+                list[min_index] = var
+    return list
 #3
 def sort_books_by_field(book, field):
     return sorted(book, key=lambda x: x[field])
+
+#4
+def large_string_insertion_sort(list):
+    large_list = len(list)
+    for i in range(large_list):
+        min_index = i
+        for j in range(i, large_list):
+            if len(list[j]) < len(list[min_index]): 
+                var = list[j]
+                list[j] = list[min_index]
+                list[min_index] = var
+    return list
+#5
+def large_string_insertion_invested(list):
+    large_list = len(list)
+    for i in range(large_list):
+        min_index = i
+        for j in range(i, large_list):
+            if len(list[j]) > len(list[min_index]): 
+                var = list[j]
+                list[j] = list[min_index]
+                list[min_index] = var
+    return list
 
 #6
 def sort_by_count(list):
@@ -17,6 +62,25 @@ def sort_by_count(list):
     for i in range(len(counter)):
         list_sort.extend([i + min_val] * counter[i])
 
+    return list_sort
+
+#7
+def items_list_sort(list):
+    large_list = len(list)
+    number_list = []
+    string_list = []
+    list_sort = []
+    for i in range(large_list):
+        if isinstance(list[i], str):
+            string_list.append(list[i])
+        elif isinstance(list[i], int):
+            number_list.append(list[i])
+    
+    number_list = numbers_bubble_sort(number_list)
+
+    string_list = caracter_selection_sort(string_list)
+
+    list_sort = number_list + string_list
     return list_sort
 
 #8
